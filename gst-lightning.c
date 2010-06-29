165f9cb7a2ff0ee21ce9b4516e9d679d6481bf30
mathieu <mathieu@mathieu-desktop.(none)>
2010-06-29 15:17:47 +0200

    Add smalltalk object and add the allocation C function

--- gst-lightning.c
+++ gst-lightning.c
@@ -1,4 +1,7 @@
 #include <lightning.h>
+#include <stdlib.h>
+
+static VMProxy *_gst_vm_proxy;
 
 void
 addi_i (jit_state *jit, char i, char j, char k)
@@ -827,4 +830,25 @@ set_ip (jit_state *jit, jit_insn * ip)
 #define _jit (*jit)
   jit_set_ip (ip);
 #undef _jit
+}
+
+/* Create a new jit_state used by gst
+ * It also allocate the code buffer of a insnSize bytes
+ */
+jit_state*
+lightning_allocate_jit_state (size_t insnSize)
+{
+	jit_state* new_jit_state;
+	jit_insn* new_pc;
+	new_pc = malloc(insnSize);
+	new_jit_state = malloc(sizeof(jit_state));
+	new_jit_state->x.pc = new_pc;
+	return new_jit_state;
+}
+
+void
+gst_initModule (proxy)
+     VMProxy *proxy;
+{
+	_gst_vm_proxy = proxy;
 }
